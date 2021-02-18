0bf5e9f7dc367dd3fef9e293e555d8762a936b5a
Cedric Wider <wider.cedric@gmail.com>
2021-02-18 10:50:23 +0100

    Calculate Namespace based on given attributes

--- src/create_namespace.ts
+++ src/create_namespace.ts
@@ -5,14 +5,19 @@ export async function createNamespace(
   guild: string,
   url: string
 ): Promise<string> {
-  const {status, data} = await axios.post(url, {
-    // when branch name is too long, API throws an error.
-    name: name.substr(0, 42),
-    guild
-  })
+  const {status, data} = await axios.post(url, {name, guild})
 
   if (status < 200 || status >= 300) {
     throw new Error(`Received non-ok status message from webservice: ${status}`)
   }
   return data.name
+}
+
+export function calculateNamespaceIdentifier(
+  appName: string,
+  branch: string,
+  revision: string
+): string {
+  const appIdentifier = `${appName}-${branch}`.substr(0, 34)
+  return `${appIdentifier}-${revision.substr(0, 7)}`
 }
